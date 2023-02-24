d04c92fb8c6b469e56b035338b8815473144305d
Jose <106718014+CodeMasterJose@users.noreply.github.com>
2023-02-24 01:03:42 -0800

    Add files via upload

---
+++ polybar/launch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+## Terminate already running bar instances 
+killall -q polybar
+#Launch bar
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar example >>/tmp/polybar1.log 2>&1 &
+
+echo "Bars launches..."
